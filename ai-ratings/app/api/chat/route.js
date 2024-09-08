a374e4d74c519fc612ddffbe295b1dde57357721
aarch34 <aarcha.u@yahoo.com>
2024-09-08 23:40:54 +0530

    need to start page.js

--- ai-ratings/app/api/chat/route.js
+++ ai-ratings/app/api/chat/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { Pinecone } from "@pinecone-database/pinecone";
-import groq from "groq";
+import Groq from "groq";
 
 const systemPrompt = 
 `
@@ -17,14 +17,14 @@ Respect student and teacher privacy by not identifying individuals.
 export async function POST(req) {
     const data = await req.json()
     const pc=new Pinecone({
-        apiKey: 'YOUR_API_KEY',
+        apiKey: '83dd7137-dace-465a-a81e-70aa4358ab3a',
     })    
     const index = pc.index('rag').name('ns1')
     const Groq = new Groq()
 
     const text = data[data.length-1].content
     const embedding = await Groq.Embeddings.create({
-        model: 'text-embedding-3-small',
+        model: 'text-embedding-ada-002',
         input:text,
         encoding_format: 'float',
     })
@@ -47,4 +47,36 @@ export async function POST(req) {
     })
     const lastMessage = data[data.length-1]
     const lastMessageContent = lastMessage.content + resultsString
+    const lastDataWithoutLastMessage = data.slice(0,data.length-1)
+    const completion = await groq.chat.completions.create({
+        messages: [
+            {role:'system',content:systemPrompt},
+            ...lastDataWithoutLastMessage,
+            {role:'user',content:lastMessageContent}
+        ],
+        model:'llama3-8b-8192',
+        stream:true,
+    })
+    const stream =ReadableStream({
+        async start(controller){
+            const encoder= new TextEncoder()
+            try{
+                for await(const chunk of completion ){
+                    const content=chunk.choices[0]?.delta?.content
+                    if(content){
+                        const text=encoder.encode(content)
+                        controller.enqueue(text)
+                    }
+                }
+            }
+            catch(err){
+                controller.err(err)
+            }
+            finally{
+                controller.close()
+            }
+        }
+    })
+    return new NextResponse(stream)
+
 }
